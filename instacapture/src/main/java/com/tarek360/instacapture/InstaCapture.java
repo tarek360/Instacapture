126d1f7d3ace371a7a6816ead1ee380751d70721
Anas Alaa <anasaladdin5@gmail.com>
2016-12-22 22:48:14 +0200

    Update project to Rxjava 2 projects consume InstaCupture with Rxjava 1 need to use https://github.com/akarnokd/RxJava2Interop

--- instacapture/src/main/java/com/tarek360/instacapture/InstaCapture.java
+++ instacapture/src/main/java/com/tarek360/instacapture/InstaCapture.java
@@ -5,179 +5,192 @@ import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+
 import com.tarek360.instacapture.exception.ActivityNotRunningException;
 import com.tarek360.instacapture.listener.ScreenCaptureListener;
 import com.tarek360.instacapture.screenshot.ScreenshotProvider;
 import com.tarek360.instacapture.utility.Logger;
-import rx.Observable;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
+
+import org.reactivestreams.Subscriber;
+
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 
 /**
  * Created by tarek on 5/17/16.
  */
 public final class InstaCapture {
-  public static long startTime;
+    public static long startTime;
 
-  private static final String MESSAGE_IS_ACTIVITY_RUNNING = "Is your activity running?";
-  private static final String MESSAGE_BUSY = "InstaCapture is busy, please wait!";
-  private static final String ERROR_INIT_WITH_DESTROYED_ACTIVITY = "Your Activity may be destroyed";
-  private static final String ERROR_SCREENSHOT_CAPTURE_FAILED = "Screenshot capture failed";
+    private static final String MESSAGE_IS_ACTIVITY_RUNNING = "Is your activity running?";
+    private static final String MESSAGE_BUSY = "InstaCapture is busy, please wait!";
+    private static final String ERROR_INIT_WITH_DESTROYED_ACTIVITY = "Your Activity may be destroyed";
+    private static final String ERROR_SCREENSHOT_CAPTURE_FAILED = "Screenshot capture failed";
 
-  private static InstaCapture instance;
-  private static Listener listener;
+    private static InstaCapture instance;
+    private static Listener listener;
 
-  @NonNull private ActivityReferenceManager activityReferenceManager;
+    @NonNull
+    private ActivityReferenceManager activityReferenceManager;
 
-  @NonNull private ScreenshotProvider screenshotProvider;
+    @NonNull
+    private ScreenshotProvider screenshotProvider;
 
-  private ScreenCaptureListener mScreenCapturingListener;
+    private ScreenCaptureListener mScreenCapturingListener;
 
-  private InstaCapture(@NonNull final Activity activity) {
+    private InstaCapture(@NonNull final Activity activity) {
 
-    this.activityReferenceManager = new ActivityReferenceManager();
-    this.activityReferenceManager.setActivity(activity);
-    this.screenshotProvider = getScreenshotProvider();
-  }
-
-  /**
-   * Get single tone instance.
-   *
-   * @param activity .
-   * @return InstaCapture single tone instance.
-   */
-
-  /**
-   * Set configuration.
-   *
-   * @param configuration InstaCaptureConfiguration.
-   */
-  public static void setConfiguration(InstaCaptureConfiguration configuration) {
-    if (configuration.logging) {
-      Logger.enable();
-    } else {
-      Logger.disable();
-    }
-  }
-
-  /** Returns singleton class instance */
-  public static InstaCapture getInstance(@NonNull final Activity activity) {
-
-    synchronized (InstaCapture.class) {
-      if (instance == null) {
-        instance = new InstaCapture(activity);
-      } else {
-        instance.setActivity(activity);
-      }
+        this.activityReferenceManager = new ActivityReferenceManager();
+        this.activityReferenceManager.setActivity(activity);
+        this.screenshotProvider = getScreenshotProvider();
     }
 
-    return instance;
-  }
+    /**
+     * Get single tone instance.
+     *
+     * @param activity .
+     * @return InstaCapture single tone instance.
+     */
 
-  private void setActivity(@NonNull final Activity activity) {
-    this.activityReferenceManager.setActivity(activity);
-  }
+    /**
+     * Set configuration.
+     *
+     * @param configuration InstaCaptureConfiguration.
+     */
+    public static void setConfiguration(InstaCaptureConfiguration configuration) {
+        if (configuration.logging) {
+            Logger.enable();
+        } else {
+            Logger.disable();
+        }
+    }
 
-  /**
-   * Capture the current screen.
-   */
-  public Listener capture() {
-    capture(null);
-    return getListener();
-  }
+    /**
+     * Returns singleton class instance
+     */
+    public static InstaCapture getInstance(@NonNull final Activity activity) {
 
-  /**
-   * Capture the current screen.
-   *
-   * @param ignoredViews from screenshot .
-   */
-  public Listener capture(View... ignoredViews) {
+        synchronized (InstaCapture.class) {
+            if (instance == null) {
+                instance = new InstaCapture(activity);
+            } else {
+                instance.setActivity(activity);
+            }
+        }
 
-    captureRx(ignoredViews).subscribe(new Subscriber<Bitmap>() {
-      @Override public void onCompleted() {
-      }
+        return instance;
+    }
 
-      @Override public void onError(final Throwable e) {
-        Logger.e(ERROR_SCREENSHOT_CAPTURE_FAILED);
-        Logger.printStackTrace(e);
+    private void setActivity(@NonNull final Activity activity) {
+        this.activityReferenceManager.setActivity(activity);
+    }
+
+    /**
+     * Capture the current screen.
+     */
+    public Listener capture() {
+        capture(null);
+        return getListener();
+    }
+
+    /**
+     * Capture the current screen.
+     *
+     * @param ignoredViews from screenshot .
+     */
+    public Listener capture(View... ignoredViews) {
+
+        captureRx(ignoredViews).subscribe(new SingleObserver<Bitmap>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onSuccess(Bitmap bitmap) {
+                if (mScreenCapturingListener != null) {
+                    mScreenCapturingListener.onCaptureComplete(bitmap);
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Logger.e(ERROR_SCREENSHOT_CAPTURE_FAILED);
+                Logger.printStackTrace(e);
+
+                if (mScreenCapturingListener != null) {
+                    mScreenCapturingListener.onCaptureFailed(e);
+                }
+            }
+        });
+        return getListener();
+    }
+
+    /**
+     * Capture the current screen.
+     *
+     * @return a Single<File>
+     */
+    public Single<Bitmap> captureRx() {
+        return captureRx(null);
+    }
+
+    /**
+     * Capture the current screen.
+     *
+     * @param ignoredViews from screenshot.
+     * @return a Single<Bitmap>
+     */
+    public Single<Bitmap> captureRx(@Nullable View... ignoredViews) {
+
+        InstaCapture.startTime = System.currentTimeMillis();
+        final Activity activity = activityReferenceManager.getValidatedActivity();
+        if (activity == null) {
+            return Single.error(new ActivityNotRunningException(MESSAGE_IS_ACTIVITY_RUNNING));
+        }
 
         if (mScreenCapturingListener != null) {
-          mScreenCapturingListener.onCaptureFailed(e);
+            mScreenCapturingListener.onCaptureStarted();
         }
-      }
 
-      @Override public void onNext(final Bitmap file) {
-        if (mScreenCapturingListener != null) {
-          mScreenCapturingListener.onCaptureComplete(file);
+        return screenshotProvider.getScreenshotBitmap(activity, ignoredViews)
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    /**
+     * @return a ScreenshotProvider
+     */
+    private ScreenshotProvider getScreenshotProvider() {
+
+        final Activity activity = activityReferenceManager.getValidatedActivity();
+        if (activity == null) {
+            Logger.e(MESSAGE_IS_ACTIVITY_RUNNING);
+            throw new IllegalArgumentException(ERROR_INIT_WITH_DESTROYED_ACTIVITY);
         }
-      }
-    });
-    return getListener();
-  }
 
-  /**
-   * Capture the current screen.
-   *
-   * @return a Observable<File>
-   */
-  public Observable<Bitmap> captureRx() {
-    return captureRx(null);
-  }
-
-  /**
-   * Capture the current screen.
-   *
-   * @param ignoredViews from screenshot.
-   * @return a Observable<Bitmap>
-   */
-  public Observable<Bitmap> captureRx(@Nullable View... ignoredViews) {
-
-    InstaCapture.startTime = System.currentTimeMillis();
-    final Activity activity = activityReferenceManager.getValidatedActivity();
-    if (activity == null) {
-      return Observable.error(new ActivityNotRunningException(MESSAGE_IS_ACTIVITY_RUNNING));
+        return new ScreenshotProvider();
     }
 
-    if (mScreenCapturingListener != null) {
-      mScreenCapturingListener.onCaptureStarted();
+    private Listener getListener() {
+
+        synchronized (Listener.class) {
+            if (listener == null) {
+                listener = new Listener();
+            }
+        }
+
+        return listener;
     }
 
-    return screenshotProvider.getScreenshotBitmap(activity, ignoredViews)
-        .observeOn(AndroidSchedulers.mainThread());
-  }
+    public final class Listener {
 
-  /**
-   * @return a ScreenshotProvider
-   */
-  private ScreenshotProvider getScreenshotProvider() {
+        private Listener() {
+        }
 
-    final Activity activity = activityReferenceManager.getValidatedActivity();
-    if (activity == null) {
-      Logger.e(MESSAGE_IS_ACTIVITY_RUNNING);
-      throw new IllegalArgumentException(ERROR_INIT_WITH_DESTROYED_ACTIVITY);
+        public void setScreenCapturingListener(@NonNull ScreenCaptureListener listener) {
+            mScreenCapturingListener = listener;
+        }
     }
-
-    return new ScreenshotProvider();
-  }
-
-  private Listener getListener() {
-
-    synchronized (Listener.class) {
-      if (listener == null) {
-        listener = new Listener();
-      }
-    }
-
-    return listener;
-  }
-
-  public final class Listener {
-
-    private Listener() {
-    }
-
-    public void setScreenCapturingListener(@NonNull ScreenCaptureListener listener) {
-      mScreenCapturingListener = listener;
-    }
-  }
 }
--- instacapture/src/main/java/com/tarek360/instacapture/screenshot/ViewsBitmapObservable.java
+++ instacapture/src/main/java/com/tarek360/instacapture/screenshot/ViewsBitmapObservable.java
@@ -5,32 +5,36 @@ import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+
 import com.tarek360.instacapture.exception.IllegalScreenSizeException;
-import rx.Observable;
-import rx.functions.Func0;
+
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
 
 /**
  * Created by tarek on 5/17/16.
  */
 public final class ViewsBitmapObservable {
 
-  private ViewsBitmapObservable() {
+    private ViewsBitmapObservable() {
 
-  }
+    }
 
-  @NonNull public static Observable<Bitmap> get(@NonNull final Activity activity,
-      @Nullable final View[] removedViews) {
+    @NonNull
+    public static Single<Bitmap> get(@NonNull final Activity activity,
+                                         @Nullable final View[] removedViews) {
 
-    return Observable.defer(new Func0<Observable<Bitmap>>() {
-      @Override public Observable<Bitmap> call() {
-
-        Bitmap screenBitmap = ScreenshotTaker.getScreenshotBitmap(activity, removedViews);
-        if (screenBitmap != null) {
-          return Observable.just(screenBitmap);
-        } else {
-          return Observable.error(new IllegalScreenSizeException());
-        }
-      }
-    });
-  }
+        return Single.create(new SingleOnSubscribe<Bitmap>() {
+            @Override
+            public void subscribe(SingleEmitter<Bitmap> e) throws Exception {
+                Bitmap screenBitmap = ScreenshotTaker.getScreenshotBitmap(activity, removedViews);
+                if (screenBitmap != null) {
+                    e.onSuccess(screenBitmap);
+                } else {
+                    e.onError(new IllegalScreenSizeException());
+                }
+            }
+        });
+    }
 }
