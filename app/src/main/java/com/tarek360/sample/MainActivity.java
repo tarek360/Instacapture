fb827c07e6f2a9661757480e1a012f5b73369e02
Ahmed Tarek <ahmed.tarek@code95.com>
2017-02-04 19:31:45 +0200

    start using kotlin :)

--- app/src/main/java/com/tarek360/sample/MainActivity.java
+++ app/src/main/java/com/tarek360/sample/MainActivity.java
@@ -1,27 +1,17 @@
 package com.tarek360.sample;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-  @BindView(R.id.sample_texture_view) Button sampleTextureView;
-
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
-
-    if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-      sampleTextureView.setVisibility(View.GONE);
-    }
   }
 
   @OnClick(R.id.sample_google_map_fragment) public void sampleGoogleMapFragment() {
@@ -46,10 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
   @OnClick(R.id.sample_custom_view) public void sampleCustomView() {
     startActivity(new Intent(this, CustomViewSampleActivity.class));
-  }
-
-  @OnClick(R.id.sample_texture_view) public void sampleTextureView() {
-    startActivity(new Intent(this, TextureViewSampleActivity.class));
   }
 
   @OnClick(R.id.sample_gl_surface_view) public void sampleGLSurfaceView() {
